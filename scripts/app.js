e075aa2ec2a5bb67c4a4e7cecfcc81c870a9b9fb
Killy Gourou <goureau.n.a@hotmail.fr>
2018-10-12 11:35:58 +0200

    Updating card generation. TODO : CORS query

--- scripts/app.js
+++ scripts/app.js
@@ -19,230 +19,107 @@
   var app = {
     isLoading: true,
     visibleCards: {},
-    selectedCities: [],
+    favoriteStop: [],
     spinner: document.querySelector('.loader'),
     cardTemplate: document.querySelector('.cardTemplate'),
-    container: document.querySelector('.main'),
-    addDialog: document.querySelector('.dialog-container'),
-    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    container: document.querySelector('.main')
   };
 
+  app.updateStopCard = function(data){
+    var name = data.libelle
+    var lignes = data.ligne.reduce((x,y)=> x +String(y.numLigne)+",", "Ligne ")
+    console.log(name,lignes)
 
+    var card = app.visibleCards[data.codeLieu]
+    if(!card){
+      card = app.cardTemplate.cloneNode(true)
+      card.classList.remove('cardTemplate');
+      card.querySelector('.card-title').textContent = data.name
+      card.removeAttribute('hidden')
+      app.container.appendChild(card)
+      app.visibleCards[data.codeLieu] = card
+    }
+  }
+
+  const initialStopData = [
+    {"codeLieu":"CRQU",
+    "libelle":"Place du Cirque",
+    "distance":"13 m",
+    "ligne":[
+      {"numLigne":"2"},
+      {"numLigne":"C1"},
+      {"numLigne":"C2"},
+      {"numLigne":"C6"},
+      {"numLigne":"11"},
+      {"numLigne":"23"},
+      {"numLigne":"LU"}
+    ]},
+    {"codeLieu":"BRTA",
+    "libelle":"Bretagne",
+    "distance":"120 m",
+    "ligne":[
+      {"numLigne":"3"}
+    ]},
+    {"codeLieu":"CMAR",
+    "libelle":"Cirque - Marais",
+    "distance":"162 m",
+    "ligne":[
+      {"numLigne":"C2"},
+      {"numLigne":"12"},
+      {"numLigne":"23"}
+    ]}
+  ]
   /*****************************************************************************
    *
    * Event listeners for UI elements
    *
    ****************************************************************************/
 
-  document.getElementById('butRefresh').addEventListener('click', function() {
-    // Refresh all of the forecasts
-    app.updateForecasts();
-  });
-
-  document.getElementById('butAdd').addEventListener('click', function() {
-    // Open/show the add new city dialog
-    app.toggleAddDialog(true);
-  });
-
-  document.getElementById('butAddCity').addEventListener('click', function() {
-    // Add the newly selected city
-    var select = document.getElementById('selectCityToAdd');
-    var selected = select.options[select.selectedIndex];
-    var key = selected.value;
-    var label = selected.textContent;
-    if (!app.selectedCities) {
-      app.selectedCities = [];
+  //TODO
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getStopFromPos);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
-    app.getForecast(key, label);
-    app.selectedCities.push({key: key, label: label});
-    app.saveSelectedCities();
-    app.toggleAddDialog(false);
-  });
+  }
 
-
-  document.getElementById('butAddCancel').addEventListener('click', function() {
-    // Close the add new city dialog
-    app.toggleAddDialog(false);
-  });
-
-
-  /*****************************************************************************
-   *
-   * Methods to update/refresh the UI
-   *
-   ****************************************************************************/
-
-  // Toggles the visibility of the add new city dialog
-
-  // Updates a weather card with the latest weather forecast. If the card
-  // doesn't already exist, it's cloned from the template.
-  app.updateForecastCard = function(data) {
-    var dataLastUpdated = new Date(data.created);
-
-    if (app.isLoading) {
-      app.spinner.setAttribute('hidden', true);
-      app.container.removeAttribute('hidden');
-      app.isLoading = false;
-    }
-  };
-
-
-  /*****************************************************************************
-   *
-   * Methods for dealing with the model
-   *
-   ****************************************************************************/
-
-  /*
-   * Gets a forecast for a specific city and updates the card with the data.
-   * getForecast() first checks if the weather data is in the cache. If so,
-   * then it gets that data and populates the card with the cached data.
-   * Then, getForecast() goes to the network for fresh data. If the network
-   * request goes through, then the card gets updated a second time with the
-   * freshest data.
-   */
-  app.getForecast = function(key, label) {
-    var statement = 'select * from weather.forecast where woeid=' + key;
-    var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
-        statement;
-    // TODO add cache logic here
-
-    // Fetch the latest data.
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-          var response = JSON.parse(request.response);
-          var results = response.query.results;
-          results.key = key;
-          results.label = label;
-          results.created = response.query.created;
-          app.updateForecastCard(results);
+  function getStopFromPos(position){
+    var lat = String(position.coords.latitude).replace('.',',');
+    var long = String(position.coords.longitude).replace('.',',');
+    getJSON("http://open.tan.fr/ewp/arrets.json/"+lat+"/"+long,
+      function(err, data) {
+        if (err !== null) {
+          alert('Something went wrong: ' + err);
+        } else {
+          alert('Your data: ' + data);
+          return data
         }
+    });
+
+  }
+
+  var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
       } else {
-        // Return the initial weather forecast since no data is available.
-        app.updateForecastCard(initialWeatherForecast);
+        callback(status, xhr.response);
       }
     };
-    request.open('GET', url);
-    request.send();
-  };
+    xhr.send();
+};
 
-  // Iterate all of the cards and attempt to get the latest forecast data
-  app.updateForecasts = function() {
-    var keys = Object.keys(app.visibleCards);
-    keys.forEach(function(key) {
-      app.getForecast(key);
-    });
-  };
-
-  // TODO add saveSelectedCities function here
-   // Save list of cities to localStorage.
-   app.saveSelectedCities = function() {
-    var selectedCities = JSON.stringify(app.selectedCities);
-    localStorage.selectedCities = selectedCities;
-  };
-
-
-  app.getIconClass = function(weatherCode) {
-    // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
-    weatherCode = parseInt(weatherCode);
-    switch (weatherCode) {
-      case 25: // cold
-      case 32: // sunny
-      case 33: // fair (night)
-      case 34: // fair (day)
-      case 36: // hot
-      case 3200: // not available
-        return 'clear-day';
-      case 0: // tornado
-      case 1: // tropical storm
-      case 2: // hurricane
-      case 6: // mixed rain and sleet
-      case 8: // freezing drizzle
-      case 9: // drizzle
-      case 10: // freezing rain
-      case 11: // showers
-      case 12: // showers
-      case 17: // hail
-      case 35: // mixed rain and hail
-      case 40: // scattered showers
-        return 'rain';
-      case 3: // severe thunderstorms
-      case 4: // thunderstorms
-      case 37: // isolated thunderstorms
-      case 38: // scattered thunderstorms
-      case 39: // scattered thunderstorms (not a typo)
-      case 45: // thundershowers
-      case 47: // isolated thundershowers
-        return 'thunderstorms';
-      case 5: // mixed rain and snow
-      case 7: // mixed snow and sleet
-      case 13: // snow flurries
-      case 14: // light snow showers
-      case 16: // snow
-      case 18: // sleet
-      case 41: // heavy snow
-      case 42: // scattered snow showers
-      case 43: // heavy snow
-      case 46: // snow showers
-        return 'snow';
-      case 15: // blowing snow
-      case 19: // dust
-      case 20: // foggy
-      case 21: // haze
-      case 22: // smoky
-        return 'fog';
-      case 24: // windy
-      case 23: // blustery
-        return 'windy';
-      case 26: // cloudy
-      case 27: // mostly cloudy (night)
-      case 28: // mostly cloudy (day)
-      case 31: // clear (night)
-        return 'cloudy';
-      case 29: // partly cloudy (night)
-      case 30: // partly cloudy (day)
-      case 44: // partly cloudy
-        return 'partly-cloudy-day';
-    }
-  };
-
-  /*
-   * Fake weather data that is presented when the user first uses the app,
-   * or when the user has not saved any cities. See startup code for more
-   * discussion.
-   */
-  var initialWeatherForecast = {
-    key: '2459115',
-    label: 'New York, NY',
-    created: '2016-07-22T01:00:00Z',
-      wind: {
-        speed: 25,
-        direction: 195
-      }
-  };
-  // TODO uncomment line below to test app with fake data
-  app.updateForecastCard(initialWeatherForecast);
-
-  // TODO add startup code here
-
-  /************************************************************************
-   *
-   * Code required to start the app
-   *
-   * NOTE: To simplify this codelab, we've used localStorage.
-   *   localStorage is a synchronous API and has serious performance
-   *   implications. It should not be used in production applications!
-   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
-   ************************************************************************/
-
-  app.selectedCities = localStorage.selectedCities;
-  if (app.selectedCities) {
-    app.selectedCities = JSON.parse(app.selectedCities);
-    app.selectedCities.forEach(function(city) {
-      app.getForecast(city.key, city.label);
+  app.favoriteStop = localStorage.favoriteStop;
+  if (app.favoriteStop) {
+    app.favoriteStop = JSON.parse(app.favoriteStop);
+    app.favoriteStop.forEach(function(stops) {
+      app.getStops(stops.id, stop.ligne);
     });
   } else {
     /* The user is using the app for the first time, or the user has not
@@ -250,11 +127,12 @@
      * scenario could guess the user's location via IP lookup and then inject
      * that data into the page.
      */
-    app.updateForecastCard(initialWeatherForecast);
-    app.selectedCities = [
-      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    var test = getLocation()
+    initialStopData.forEach(app.updateStopCard)
+    app.favoriteStop = [
+      {key: initialStopData.id, label: initialStopData.ligne}
     ];
-    app.saveSelectedCities();
+    app.savefavoriteStop();
   }
 
 
